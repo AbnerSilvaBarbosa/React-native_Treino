07b2fe4d81f6ab0954143be4c1ed81387841ad51
abnerSilvaBarbosa <abner.barbosa@sou.inteli.edu.br>
2023-01-19 19:43:14 -0300

    The firsts learns Expo Pouncival Core Components Handling text input ScrollView List Views Plataform Specific Code

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, Image, ScrollView, SectionList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState("")
+
+  const cursos = [{ curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" }, { curso: "JS" },]
+  const cursos_ = [{ title: "C", data: ["C", "C++", "C#"] }, { title: "J", data: ["JS", "Java"] },]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput
+          placeholder='Escreva seu nome aqui'
+          onChangeText={(e) => setText(e)}
+
+        />
+      </View>
+
+      <View style={styles.block}>
+        <Text style={styles.nameText}>{text}</Text>
+      </View>
+
+
+      {/* <View>
+        <FlatList
+          data={cursos}
+          renderItem={({ item }) => <Text style={{ color: "#1f1f1f" }}>{item.curso}</Text>}
+
+        >
+        </FlatList>
+      </View> */}
+
+      <View style={{
+        flex: 1,
+        paddingTop: 22,
+      }}>
+
+        <SectionList
+          sections={cursos_}
+          renderItem={({ item }) => <Text style={{ color: "#1f1f1f" }}>{item}</Text>}
+          renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={item => `basicListEntry-${item}`}
+        >
+
+        </SectionList>
+      </View>
+
+
+
     </View>
   );
 }
@@ -13,8 +57,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100
+
+  },
+
+  block: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1f1f1f",
+    height: 80,
+    width: 200
+  },
+
+  nameText: {
+    color: "#ffffff"
+  },
+
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
 });
